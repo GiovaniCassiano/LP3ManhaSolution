a8152c715902439e2079ad4148cbd8a88785df2b
GiovaniCassiano <giovani.cassiano99@hotmail.com>
2016-08-19 10:56:16 -0300

    CSS

---
+++ TodoWeb/Models/Tarefa.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TodoWeb.Models
+{
+    public class Tarefa
+    {
+        public int TarefaID { get; set; }
+
+        public string Nome { get; set; }
+
+        public bool Concluida { get; set; }
+
+        public int UsuarioID { get; set; }
+
+        public int CategoriaID { get; set; }
+
+        public string Descricao { get; set; }
+    }
+}
